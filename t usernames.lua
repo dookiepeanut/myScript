9d7c6b3cf79c744a8e3e3e6fc24cdaac74c6387e
dookiepeanut <vpstempmail@gmail.com>
2025-02-25 16:19:38 +0800

    Update t usernames.lua

--- t usernames.lua
+++ t usernames.lua
@@ -1,21 +1,7 @@
 local usernames = {
-"Vapor_Vortex34CmZF4",
 "Data99HU92ns",
 "Atomic_AvatarXCayN",
-"Infra_Matrixl",
-"VaporKuKmoVy",
-"Solar_Network97tJT",
-"Cyber_RiderYoSH2",
-"InfraPhaser797352yh",
-"ElectriciMH82",
-"ZonecPceaH",
-"NetworkPUiyB74Rh0I3",
-"Stellar_Vortex46XCg",
 "Neon7Zv1f",
-"ElectroNinja_3f6Mz",
-"Binary197xdXpIbWCaa",
-"HyperY6M",
 "CircuitBgz2GLr9d5",
-"DronexxNU",
 }
 return usernames
