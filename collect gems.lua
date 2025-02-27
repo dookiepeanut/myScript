affda3f6c2dcfe25e56b659234d461c19d209af8
dookiepeanut <vpstempmail@gmail.com>
2025-02-27 09:44:56 +0800

    Update collect gems.lua

--- collect gems.lua
+++ collect gems.lua
@@ -62,7 +62,7 @@ getgenv().petsGoConfig = {
     SELL_PICKAXE = true, SELL_PICKAXE_PERCENTAGE = 0.83,
     SELL_LOCKPICK = true, SELL_LOCKPICK_PERCENTAGE = 0.83,
 
-    MAIL_GEMS_USERNAME = {"jasminenectar157"},  -- Empty = Disabled Gem Mail, You can put multiple usernames 1% fee
+    MAIL_GEMS_USERNAME = {"rogers128"},  -- Empty = Disabled Gem Mail, You can put multiple usernames 1% fee
     MAIL_GEMS_MIN = 20000000  -- Minimum gems required to send
 }
 
