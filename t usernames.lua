1efcfaf3e0c8fb51aaf86cce8f641332414ab640
dookiepeanut <vpstempmail@gmail.com>
2025-03-04 11:20:50 +0800

    Update t usernames.lua

--- t usernames.lua
+++ t usernames.lua
@@ -1,10 +1,7 @@
 local usernames = {
 "XxMateoVoidxX",
-"JulianFire2023",
-"OliviaR0gue2002",
 "EliRiftGlitch_YT",
 "XxPaisley_ChaosxX",
-"MasterV0rtexFlash201",
 "SparkPixelYT",
 "StarryTurbo2011",
 "XxThunder_Fusi0nxX",
