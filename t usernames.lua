233c0f6d87a27c3c5248399342a638d6f091217d
dookiepeanut <vpstempmail@gmail.com>
2025-03-07 20:46:31 +0800

    Update t usernames.lua

--- t usernames.lua
+++ t usernames.lua
@@ -1,15 +1,11 @@
 local usernames = {
   "ArcturusAshdrake6174",
   "AstraluxAstra7564",
-  "OrionKnight359",
-  "GalacticAstra9886",
   "GoliathNightbloom942",
   "MarinerRiddle3305",
   "DawnflareAstral9017",
   "MirageSnowy6129",
-  "FlowTorchbearer3030",
   "ZenithalCinder9378",
   "CalmNightroar2520",
-  "StormcloudVortex5667",
 }
 return usernames
